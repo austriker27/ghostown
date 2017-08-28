72cbfdf589b9522c02c72c3f1674ac2fb9b7a8ec
MAX MCFARLAND <mmcfarland522@gmail.com>
2017-08-28 13:33:49 -0700

    linked page fixes

--- home.js
+++ home.js
@@ -1,18 +1,20 @@
-function Character (name, str, agil, int, hp){
+function Character (name, str, agil, int, hp, ghostType){
   this.name = name;
   this.str = str;
   this.agil = agil;
   this.int = int;
   this.hp = hp;
-  this.totalLvl = (this.str + this.agil + this.int);
+  this.ghostType = 0;
+  this.totalLvl = 0;
 }
-var userChar = '';
+var userChar = [];
 for(var i = 0; i < document.getElementsByClassName('ghostPic').length; i++){
   var ghostie = document.getElementsByClassName('ghostPic')[i];
   ghostie.addEventListener('click', selectGhost);
 }
 function selectGhost(event){
-  var userChar = event.target.id;
+  userChar = [];
+  userChar.push(event.target.id);
   if(document.getElementsByClassName('chooseGhostie')[0]){
     var chooseText = document.getElementsByClassName('chooseGhostie')[0];
     var parentElm = chooseText.parentNode;
@@ -35,41 +37,46 @@ function selectGhost(event){
 
 function genStats(max, min, stat){
   var statCreate = Math.floor(Math.random() * (max - min) + min);
-  return stat;
+  return statCreate;
 }
 
 function startGame(event){
-  var userName = event.target.input;
-  var mainCharacter = new Character(userName, 0, 0, 0, 0);
-  if(userChar == 'ghostOne'){
-    var strGen = genStats(4, 3);
+  event.preventDefault();
+  var userName = event.target.nameForm.value;
+  var mainCharacter = new Character(userName, 0, 0, 0, 0, 0);
+  if(userChar[0] == 'ghostOne'){
+    var strGen = genStats(5, 3);
     mainCharacter.str = strGen;
-    var intGen = genStats(2, 1);
+    var intGen = genStats(3, 1);
     mainCharacter.int = intGen;
-    var agilGen = genStats(6, 5);
+    var agilGen = genStats(7, 5);
     mainCharacter.agil = agilGen;
-    var hpGen = genStats(11, 9);
+    var hpGen = genStats(12, 9);
     mainCharacter.hp = hpGen;
-  }else if(userChar == 'ghostTwo'){
-    var strGen = genStats(2, 1);
+    mainCharacter.ghostType = 0;
+  }else if(userChar[0] == 'ghostTwo'){
+    var strGen = genStats(3, 1);
     mainCharacter.str = strGen;
-    var intGen = genStats(6, 5);
+    var intGen = genStats(7, 5);
     mainCharacter.int = intGen;
-    var agilGen = genStats(4, 3);
+    var agilGen = genStats(5, 3);
     mainCharacter.agil = agilGen;
-    var hpGen = genStats(10, 8);
+    var hpGen = genStats(11, 8);
     mainCharacter.hp = hpGen;
-  }else if(userChar == 'ghostThree'){
-    var strGen = genStats(8, 7);
+    mainCharacter.ghostType = 1;
+  }else if(userChar[0] == 'ghostThree'){
+    var strGen = genStats(9, 7);
     mainCharacter.str = strGen;
-    var intGen = genStats(2, 1);
+    var intGen = genStats(3, 1);
     mainCharacter.int = intGen;
-    var agilGen = genStats(2, 1);
+    var agilGen = genStats(3, 1);
     mainCharacter.agil = agilGen;
-    var hpGen = genStats(11, 9);
+    var hpGen = genStats(14, 11);
     mainCharacter.hp = hpGen;
+    mainCharacter.ghostType = 2;
   }
+  mainCharacter.totalLvl = (mainCharacter.str + mainCharacter.int + mainCharacter.agil + mainCharacter.hp);
   console.log(mainCharacter.totalLvl);
   localStorage.setItem('character', JSON.stringify(mainCharacter));
-  window.location.replace(game.html);
+  window.open('game.html','_self');
 }
