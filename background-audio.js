c0c32d8fa924002a5ef203fd56fa0e284af9f49f
austriker27 <davidalindahl@gmail.com>
2017-08-28 16:17:30 -0700

    completed html play pause button

---
+++ background-audio.js
@@ -0,0 +1,13 @@
+var music = document.getElementById('audio');
+
+function playAudio() {
+  if (music.paused) {
+    music.play();
+    pButton.className = '';
+    pButton.className = 'pause';
+  } else {
+    music.pause();
+    pButton.className = '';
+    pButton.className = 'play';
+  }
+}
