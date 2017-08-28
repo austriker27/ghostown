4d91b0c5de6bcabec0eae740e399eaa5a75cef63
austriker27 <davidalindahl@gmail.com>
2017-08-28 16:40:45 -0700

    adding audio.js

--- audio.js
+++ audio.js
@@ -5,11 +5,9 @@ var music = document.getElementById('audio');
 function playAudio() {
   if (music.paused) {
     music.play();
-    pButton.className = '';
     pButton.className = 'pause';
   } else {
     music.pause();
-    pButton.className = '';
     pButton.className = 'play';
   }
 }
