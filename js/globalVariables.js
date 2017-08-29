ef265d372a36b2f30e08617283cf22aaa4ddfc74
MAX MCFARLAND <mmcfarland522@gmail.com>
2017-08-29 15:44:54 -0700

    MVP JS

--- js/globalVariables.js
+++ js/globalVariables.js
@@ -11,15 +11,18 @@ function Character (name, str, agil, int, hp, ghostType){
 }
 var questionCounter = 0;
 var enemyCharacter = [];
-questionTextArray = ['This is the Question. Answer it or face the consequences', 'Welcome to question two, your life will be taken from you (again), unless you answer this one correct'];
-strOptionArray = ['This is the strength Option', 'This is the strength Option'];
-intOptionArray = ['this is the Intelligence Option', 'this is the Intelligence Option'];
-agilOptionArray = ['This is the agility option', 'This is the agility option'];
-enemyEnvironmentArray = [true, true, false];
+questionTextArray = ['This is the Dog encoutner. Fight him', 'You encounter a hipster ghost. He annoyingly bumps into you. Your life will be taken from you (again), unless you answer this one correct', 'A skeleton approaches. You see a skeleton trap on the ground. What do?', 'This is the skeleton encounter. Choose your path', 'This is the second environment event. Choose wisely', 'Oh god, a cute yet legally differentialble character named ghostly approaches. What will you do to vanquish this foe?', 'Welp, this is it. It doesnt look like your going to make it out of this one. That guy looks real big. You remind yourself that youve died once before, which calms you down momentarily, anticipating the impending ass kicking. Your move'];
+strOptionArray = ['This is the strength Option', 'This is the strength Option', 'This is the strength Option', 'This is the strength Option', 'This is the strength Option', 'This is the strength Option', 'This is the strength Option'];
+intOptionArray = ['this is the Intelligence Option', 'this is the Intelligence Option', 'this is the Intelligence Option', 'this is the Intelligence Option', 'this is the Intelligence Option', 'this is the Intelligence Option', 'this is the Intelligence Option'];
+agilOptionArray = ['This is the agility option', 'This is the agility option', 'This is the agility option', 'This is the agility option', 'This is the agility option', 'This is the agility option', 'This is the agility option'];
+enemyEnvironmentArray = [true, true, false, true, false, true, true];
 
-var Doggo = new Character('Doggo', 1, 1, 1, 1);
-var hipsterGhost = new Character('Hipster Ghost', 1, 1, 1, 1);
+var Doggo = new Character('Doggo', 1, 4, 1, 10);
+var hipsterGhost = new Character('Hipster Ghost', 8, 4, 3, 15);
 var enviroOne = new Character('Skeleton Trap', 0, 1, 2, 1);
+var Skeleton = new Character('Skeleton', 10, 7, 5, 20);
 var enviroTwo = new Character('Second Environment', 3, 4, 5, 1);
-enemyCharacter.push(Doggo, hipsterGhost, enviroOne);
+var ghostly = new Character('Ghostly', 7, 14, 12, 22);
+var finalBoss = new Character('Chaos Lord', 20, 13, 17, 30);
+enemyCharacter.push(Doggo, hipsterGhost, enviroOne, Skeleton, enviroTwo, ghostly, finalBoss);
 // TODO:
