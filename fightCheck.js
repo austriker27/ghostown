6b29916ef75e921e30a182bf7e81e577ce59d262
MAX MCFARLAND <mmcfarland522@gmail.com>
2017-08-28 18:43:49 -0700

    made logic for death of enemy, and made link to second question

--- fightCheck.js
+++ fightCheck.js
@@ -1,4 +1,13 @@
-
+function Character (name, str, agil, int, hp, ghostType){
+  this.name = name;
+  this.str = str;
+  this.agil = agil;
+  this.int = int;
+  this.hp = hp;
+  this.ghostType = 0;
+  this.totalLvl = 0;
+}
+questionCounter = 0;
 function fightCheck (charOne, charTwo, stat){
   if('str' == stat){
     var damage = (charOne.str - charTwo.str);
@@ -36,15 +45,46 @@ function fightCheck (charOne, charTwo, stat){
 }
 
 // Need global user name to display user death
-// function deathCheck(charOne, charTwo){
-//   if (charTwo.hp <= 0){
-//     var display = charTwo.name + ' soul is at rest. R.I.P';
-//   }
-//   if (charOne.hp <= 0){
-//     var display = 'Oh No! You perished. Your soul moves on to a better realm.';
-//   }
-// }
+function deathCheck(charOne, charTwo){
+  if (charTwo.hp <= 0 && userCharacter.hp > 0){
+    var display = charTwo.name + ' soul is at rest. R.I.P';
+    deathDisplay(display);
+    setTimeout(function(){
+      questionCounter += 1;
+      changeQuestion(questionTextArray[questionCounter], strOptionArray[questionCounter], intOptionArray[questionCounter], agilOptionArray[questionCounter]);}, 4000);
+  }
+  else if (charOne.hp <= 0 && userCharacter.hp > 0){
+    var display = charTwo.name + ' soul is at rest. R.I.P';
+    deathDisplay(display);
+  }else if (userCharacter <= 0 ){
+    var display = 'Oh no, ' + userCharacter.name + ' died.... Better luck next time!';
+    deathDisplay(display);
+  }
 
+}
+function deathDisplay(display){
+  var textField = document.getElementsByClassName('textField')[0];
+  while (textField.hasChildNodes()) {
+    textField.removeChild(textField.lastChild);
+  };
+  var speakingField = document.createElement('h2');
+  textField.appendChild(speakingField);
+  var promptField = document.createElement('p');
+  promptField.innerText = display;
+  textField.appendChild(promptField);
+  var unList = document.createElement('ul');
+  textField.appendChild(unList);
+  var choiceOne = document.createElement('li');
+  choiceOne.innerText = 'Continue';
+  choiceOne.setAttribute('id', 'strChoice');
+  textField.appendChild(choiceOne);
+  choiceOne.addEventListener('click', continueToQuestion);
+}
+function continueToQuestion(){
+  clearTimeout();
+  questionCounter += 1;
+  changeQuestion(questionTextArray[questionCounter], strOptionArray[questionCounter], intOptionArray[questionCounter], agilOptionArray[questionCounter]);
+}
 // var Max = new Character('Max', 3, 3, 3, 3);
 // var Enemy = new Character('Blarg', 2, 4, 3, 5);
 // fightCheck(Max, Enemy, 'agil');
