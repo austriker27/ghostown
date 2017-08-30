7a381fb9360132731dee89749569863d90c55802
MAX MCFARLAND <mmcfarland522@gmail.com>
2017-08-30 10:44:05 -0700

    Added items page, made items constructor, added enviromentlogic page, cleaned fightcheck page

--- js/fightCheck.js
+++ js/fightCheck.js
@@ -16,113 +16,127 @@ function fightCheckStatType(inputStat, compareStat, charOne, charTwo){
         charTwo.hp -= damage;
         var display = charTwo.name + ' took ' + damage + ' points of damage.';
         damageDisplay(display);
-        // setTimeout(function(){
-        //   changeQuestion(questionTextArray[questionCounter], strOptionArray[questionCounter], intOptionArray[questionCounter], agilOptionArray[questionCounter]);
-        // }, 2000);
       }else {
         charOne.hp += damage;
         var display = 'Oh no, ' + charTwo.name + ' has a higher ' + inputStat + ' level! ' + charOne.name + ' took ' + (-1 * damage) + ' points of damage.';
-        // setTimeout(function(){
-        //   changeQuestion(questionTextArray[questionCounter], strOptionArray[questionCounter], intOptionArray[questionCounter], agilOptionArray[questionCounter]);
-        // }, 2000);questionTextArray
         damageDisplay(display);
       }
-    } else{ //if (encounterArray[questionCounter][5] === false) {
-      if (charTwo[inputStat] === 500){
-        if(charOne[inputStat] > 7){
-          var display = 'You stand your ground and prepare to fight.';
-          displayText(display);
-        }else{
-          charOne.hp -= 1;
-          var display = 'You attempt to stand your ground, but you are too weak. You take some damage and prepare to fight';
-          //hurt sound effect
-          displayText(display);
-        }
-      }
-      else if (charTwo[inputStat] === 501) {
-        if(charOne[inputStat] > 6){
-          questionCounter += 1;
-          var display = 'You manage to get away from the skeleton! Onward!';
-          displayText(display);
-          //skip encounter, no level up
-        }else{
-          var display = 'You attempt to run away from the skeleton, but fail. Prepare to fight';
-          displayText(display);
-        }
-      }
-      else if (charTwo[inputStat] === 502) {
-        if(charOne[inputStat] > 6){
-          enemyCharacter[questionCounter + 1].hp -= 1;
-          var display = 'Relying on your quick thinking, you manage to set the trap. The skeleton stumbles into the trap and takes some damage. Prepare to fight';
-          displayText(display);
-        }else{
-          charOne.hp -= 1;
-          var display = 'Due to your weaker intellect, in attempting to set the trap, you hurt yourself. Prepare to fight';
-          displayText(display);
-        }
-      }
-      else if (charTwo[inputStat] === 503) {
-        if(charOne[inputStat] > 10){
-          enemyCharacter[questionCounter + 1].hp -= 3;
-          var display = 'You dig in your heels and successfully make the first strike on ghostly.';
-          displayText(display);
-        }else{
-          charOne.hp -= 2;
-          var display = 'You attempt to dig in your heels, but realize that you dont have feet, causing you to fall and take damage. Prepare to fight';
-          //hurt sound effect
-          displayText(display);
-        } }
-      else if (charTwo[inputStat] === 504) {
-        enemyCharacter[questionCounter + 1].hp -= 3;
-        var display = 'You sick doggo on ghostly. Doggo, being the good dog that he is, damages ghostly. Ghostly then charges at you';
-        displayText(display);
-      }
-      else if (charTwo[inputStat] === 505) {
-        if(charOne[inputStat] > 10){
-          enemyCharacter[questionCounter + 1].hp -= 3;
-          var display = 'You taunt ghostly, causing him to faulter and take some damage. Ghostly then charges you in a fit of rage';
-          displayText(display);
-        }else{
-          charOne.int -= 2;
-          var display = 'Your attempt to taunt the ghost, but fail spectaularly. You feel your intelligence has been damaged by your stupidity. Prepare to fight';
-          //hurt sound effect
-          displayText(display);
-        }
-      }else if (charTwo[inputStat] === 506){
-        charOne.agil += 1;
-        charOne.str += 2;
-        displayText('You pick up the straight razor, feeling like you can take on the world, which you were a fan of before everyone else.');
-      }else if(charTwo[inputStat] === 507){
-        charOne.int -= 2;
-        charOne.agil += 2;
-        displayText('You put the ear gauges in your ears. You feel more aerodynamic. You also think they look dumb');
-      }else if(charTwo[inputStat] === 508){
-        charOne.int += 3;// 2-3
-        displayText('You begin to read the works of Franz Kafka. You suddenly feel like you\'re better than everyone else.');
-      }else if(charTwo[inputStat] === 509){
-        charOne.str += 6;//5-6
-        charOne.agil -= 3;//2-3
-        displayText('The hammer feels good in your hands, but is quite heavy');
-      }else if (charTwo[inputStat] === 5010){
-        charOne.int += 4;//3-4
-        charOne.agil -= 2;//1-2
-        displayText('You convene with the skull, gaining its former life\'s wisdom. It\'s quite cumbersome.');
-      }else if(charTwo[inputStat] === 5011){
-        charOne.hp += 4;
-        displayText('You eat the apple and feel better. You\'re not a huge fan of granny smiths though.');
-      }else if(charTwo[inputStat] === 5012){
-        charOne.hp -= 3;
-        displayText('Why are you touching strange clouds??? You should have eaten the apple. You feel weaker.');
-      }else if(charTwo[inputStat] === 5013){
-        charOne.hp += 6;
-        displayText('You eat the fuji apple, which you find delicious.');
-      }else if(charTwo[inputStat] === 5014){
-        charOne.agil += 4;//3-4
-        displayText('You shred some gnar. Your the fasted ghost in this town.');
-      }
+    }
+    else if (charTwo[inputStat] > 499 && charTwo[inputStat] < 506){
+      enviroEventFunct(inputStat, compareStat, charOne, charTwo);}
+    else{
+      itemEventFunction(inputStat, compareStat, charOne, charTwo);
     }
   }
 }
+      // if (charTwo[inputStat] === 500){
+      //   if(charOne[inputStat] > 7){
+      //     displayText('You stand your ground and prepare to fight.');
+      //   }else{
+      //     charOne.hp -= 1;
+      //     //hurt sound effect
+      //     displayText('You attempt to stand your ground, but you are too weak. You take some damage and prepare to fight');
+      //   }
+      // }
+      // else if (charTwo[inputStat] === 501) {
+      //   if(charOne[inputStat] > 6){
+      //     questionCounter += 1;
+      //     displayText('You manage to get away from the skeleton! Onward!');
+      //     //skip encounter, no level up
+      //   }else{
+      //     displayText('You attempt to run away from the skeleton, but fail. Prepare to fight');
+      //   }
+      // }
+      // else if (charTwo[inputStat] === 502) {
+      //   if(charOne[inputStat] > 6){
+      //     enemyCharacter[questionCounter + 1].hp -= 1;
+      //     displayText('Relying on your quick thinking, you manage to set the trap. The skeleton stumbles into the trap and takes some damage. Prepare to fight');
+      //   }else{
+      //     charOne.hp -= 1;
+      //     displayText('Due to your weaker intellect, in attempting to set the trap, you hurt yourself. Prepare to fight');
+      //   }
+      // }
+      // else if (charTwo[inputStat] === 503) {
+      //   if(charOne[inputStat] > 10){
+      //     enemyCharacter[questionCounter + 1].hp -= 3;
+      //     displayText('You dig in your heels and successfully make the first strike on ghostly.');
+      //   }else{
+      //     charOne.hp -= 2;
+      //     //hurt sound effect
+      //     displayText('You attempt to dig in your heels, but realize that you dont have feet, causing you to fall and take damage. Prepare to fight');
+      //   } }
+      // else if (charTwo[inputStat] === 504) {
+      //   enemyCharacter[questionCounter + 1].hp -= 3;
+      //   displayText('You sick doggo on ghostly. Doggo, being the good dog that he is, damages ghostly. Ghostly then charges at you');
+      // }
+      // else if (charTwo[inputStat] === 505) {
+      //   if(charOne[inputStat] > 10){
+      //     enemyCharacter[questionCounter + 1].hp -= 3;
+      //     displayText('You taunt ghostly, causing him to faulter and take some damage. Ghostly then charges you in a fit of rage');
+      //   }else{
+      //     charOne.int -= 2;
+      //     //hurt sound effect
+      //     displayText('Your attempt to taunt the ghost, but fail spectaularly. You feel your intelligence has been damaged by your stupidity. Prepare to fight');
+      //   }
+      //}
+//       else if (charTwo[inputStat] === 506){
+//         userCharacter[razor.posSkill] += razor.buff;
+//         userCharacter[razor.negSkill] -= razor.debuff;
+//         userCharacter[razor.secPosSkill] += razor.secBuff;
+//         userCharacter.itemArray.push(razor.imgNum);
+//         displayText('You pick up the straight razor, feeling like you can take on the world, which you were a fan of before everyone else.');
+//       }else if(charTwo[inputStat] === 507){
+//         userCharacter[gauges.posSkill] += gauges.buff;
+//         userCharacter[gauges.negSkill] -= gauges.debuff;
+//         userCharacter[gauges.secPosSkill] += gauges.secBuff;
+//         userCharacter.itemArray.push(gauges.imgNum);
+//         displayText('You put the ear gauges in your ears. You feel more aerodynamic. You also think they look dumb');
+//       }else if(charTwo[inputStat] === 508){
+//         userCharacter[kafka.posSkill] += kafka.buff;
+//         userCharacter[kafka.negSkill] -= kafka.debuff;
+//         userCharacter[kafka.secPosSkill] += kafka.secBuff;
+//         userCharacter.itemArray.push(kafka.imgNum);
+//         displayText('You begin to read the works of Franz Kafka. You suddenly feel like you\'re better than everyone else.');
+//       }else if(charTwo[inputStat] === 509){
+//         userCharacter[hammer.posSkill] += hammer.buff;
+//         userCharacter[hammer.negSkill] -= hammer.debuff;
+//         userCharacter[hammer.secPosSkill] += hammer.secBuff;
+//         userCharacter.itemArray.push(hammer.imgNum);
+//         displayText('The hammer feels good in your hands, but is quite heavy');
+//       }else if (charTwo[inputStat] === 5010){
+//         userCharacter[skull.posSkill] += skull.buff;
+//         userCharacter[skull.negSkill] -= skull.debuff;
+//         userCharacter[skull.secPosSkill] += skull.secBuff;
+//         userCharacter.itemArray.push(skull.imgNum);
+//         displayText('You convene with the skull, gaining its former life\'s wisdom. It\'s quite cumbersome.');
+//       }else if(charTwo[inputStat] === 5011){
+//         userCharacter[grannyApple.posSkill] += grannyApple.buff;
+//         userCharacter[grannyApple.negSkill] -= grannyApple.debuff;
+//         userCharacter[grannyApple.secPosSkill] += grannyApple.secBuff;
+//         userCharacter.itemArray.push(grannyApple.imgNum);
+//         displayText('You eat the apple and feel better. You\'re not a huge fan of granny smiths though.');
+//       }else if(charTwo[inputStat] === 5012){
+//         userCharacter[gasCloud.posSkill] += gasCloud.buff;
+//         userCharacter[gasCloud.negSkill] -= gasCloud.debuff;
+//         userCharacter[gasCloud.secPosSkill] += gasCloud.secBuff;
+//         userCharacter.itemArray.push(gasCloud.imgNum);
+//         displayText('Why are you touching strange clouds??? You should have eaten the apple. You feel weaker.');
+//       }else if(charTwo[inputStat] === 5013){
+//         userCharacter[fujiApple.posSkill] += fujiApple.buff;
+//         userCharacter[fujiApple.negSkill] -= fujiApple.debuff;
+//         userCharacter[fujiApple.secPosSkill] += fujiApple.secBuff;
+//         userCharacter.itemArray.push(fujiApple.imgNum);
+//         displayText('You eat the fuji apple, which you find delicious.');
+//       }else if(charTwo[inputStat] === 5014){
+//         userCharacter[skateboard.posSkill] += skateboard.buff;
+//         userCharacter[skateboard.negSkill] -= skateboard.debuff;
+//         userCharacter[skateboard.secPosSkill] += skateboard.secBuff;
+//         userCharacter.itemArray.push(skateboard.imgNum);
+//         displayText('You shred some gnar. Your the fasted ghost in this town.');
+//       }
+//     }
+//   }
+// }
 function damageDisplay(display){
   var textField = document.getElementsByClassName('textField')[0];
   while (textField.hasChildNodes()) {
---
+++ js/items.js
@@ -0,0 +1,76 @@
+function ItemConstructor(buff, posSkill, debuff, negSkill, imgNum, secBuff, secPosSkill){
+  this.buff = buff;
+  this.posSkill = posSkill;
+  this.debuff = debuff;
+  this.negSkill = negSkill;
+  this.imgNum = imgNum;
+  this.secBuff = secBuff;
+  this.secPosSkill = secPosSkill;
+}
+var razor = new ItemConstructor(1, 'agil',0,null,0,2,'str');
+var kafka = new ItemConstructor(3, 'int', 0, null, 1);
+var gauges = new ItemConstructor(2, 'agil', 2, 'int', 2);
+var hammer = new ItemConstructor(6, 'str', 3, 'agil', 3);
+var skull = new ItemConstructor(4, 'int', 2, 'agil', 4);
+var grannyApple = new ItemConstructor(4, 'hp', 0, null, 5);
+var gasCloud = new ItemConstructor(0, null, 3, 'hp', 6);
+var fujiApple = new ItemConstructor(6, 'hp', 0, null, 7);
+var skateboard = new ItemConstructor(4, 'agil', 0, null, 8);
+
+function itemEventFunction (inputStat, compareStat, charOne, charTwo){
+  if (charTwo[inputStat] === 506){
+    userCharacter[razor.posSkill] += razor.buff;
+    userCharacter[razor.negSkill] -= razor.debuff;
+    userCharacter[razor.secPosSkill] += razor.secBuff;
+    userCharacter.backpack.push(razor.imgNum);
+    displayText('You pick up the straight razor, feeling like you can take on the world, which you were a fan of before everyone else.');
+  }else if(charTwo[inputStat] === 507){
+    userCharacter[gauges.posSkill] += gauges.buff;
+    userCharacter[gauges.negSkill] -= gauges.debuff;
+    userCharacter[gauges.secPosSkill] += gauges.secBuff;
+    userCharacter.backpack.push(gauges.imgNum);
+    displayText('You put the ear gauges in your ears. You feel more aerodynamic. You also think they look dumb');
+  }else if(charTwo[inputStat] === 508){
+    userCharacter[kafka.posSkill] += kafka.buff;
+    userCharacter[kafka.negSkill] -= kafka.debuff;
+    userCharacter[kafka.secPosSkill] += kafka.secBuff;
+    userCharacter.backpack.push(kafka.imgNum);
+    displayText('You begin to read the works of Franz Kafka. You suddenly feel like you\'re better than everyone else.');
+  }else if(charTwo[inputStat] === 509){
+    userCharacter[hammer.posSkill] += hammer.buff;
+    userCharacter[hammer.negSkill] -= hammer.debuff;
+    userCharacter[hammer.secPosSkill] += hammer.secBuff;
+    userCharacter.backpack.push(hammer.imgNum);
+    displayText('The hammer feels good in your hands, but is quite heavy');
+  }else if (charTwo[inputStat] === 5010){
+    userCharacter[skull.posSkill] += skull.buff;
+    userCharacter[skull.negSkill] -= skull.debuff;
+    userCharacter[skull.secPosSkill] += skull.secBuff;
+    userCharacter.backpack.push(skull.imgNum);
+    displayText('You convene with the skull, gaining its former life\'s wisdom. It\'s quite cumbersome.');
+  }else if(charTwo[inputStat] === 5011){
+    userCharacter[grannyApple.posSkill] += grannyApple.buff;
+    userCharacter[grannyApple.negSkill] -= grannyApple.debuff;
+    userCharacter[grannyApple.secPosSkill] += grannyApple.secBuff;
+    userCharacter.backpack.push(grannyApple.imgNum);
+    displayText('You eat the apple and feel better. You\'re not a huge fan of granny smiths though.');
+  }else if(charTwo[inputStat] === 5012){
+    userCharacter[gasCloud.posSkill] += gasCloud.buff;
+    userCharacter[gasCloud.negSkill] -= gasCloud.debuff;
+    userCharacter[gasCloud.secPosSkill] += gasCloud.secBuff;
+    userCharacter.backpack.push(gasCloud.imgNum);
+    displayText('Why are you touching strange clouds??? You should have eaten the apple. You feel weaker.');
+  }else if(charTwo[inputStat] === 5013){
+    userCharacter[fujiApple.posSkill] += fujiApple.buff;
+    userCharacter[fujiApple.negSkill] -= fujiApple.debuff;
+    userCharacter[fujiApple.secPosSkill] += fujiApple.secBuff;
+    userCharacter.backpack.push(fujiApple.imgNum);
+    displayText('You eat the fuji apple, which you find delicious.');
+  }else if(charTwo[inputStat] === 5014){
+    userCharacter[skateboard.posSkill] += skateboard.buff;
+    userCharacter[skateboard.negSkill] -= skateboard.debuff;
+    userCharacter[skateboard.secPosSkill] += skateboard.secBuff;
+    userCharacter.backpack.push(skateboard.imgNum);
+    displayText('You shred some gnar. Your the fasted ghost in this town.');
+  }
+}
