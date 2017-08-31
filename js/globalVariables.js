42b548a7b80703342bc0b526c344870d4681784b
austriker27 <davidalindahl@gmail.com>
2017-08-31 15:12:20 -0700

    added method to character for background sounds

--- js/globalVariables.js
+++ js/globalVariables.js
@@ -1,6 +1,6 @@
 var userCharacter = JSON.parse(localStorage.character);
 
-function Character (name, str, agil, int, hp, enemy, ghostType){
+function Character (name, str, agil, int, hp, enemy, ghostType, audioFilePath){
   this.name = name;
   this.str = str;
   this.agil = agil;
@@ -9,6 +9,13 @@ function Character (name, str, agil, int, hp, enemy, ghostType){
   this.ghostType = ghostType;
   this.totalLvl = 0;
   this.enemy = enemy;
+  this.audioFilePath = audioFilePath;
+  this.enemyBackgroundMusicChange = function() {
+    var grabBackgroundMusicAudio = document.getElementById('audio');
+    var enemyAudioFile = this.audioFilePath;
+    grabBackgroundMusicAudio.setAttribute('src', this.audioFilePath);
+  };
+  this.enemyBackgroundMusicChange();
 }
 
 var enemyTurn = false;
@@ -26,16 +33,16 @@ var ghostlyEncounter = ['Oh god, a cute yet legally differentialble character na
 var thirdDropEncounter = ['The ghostly ghost drops a couple more items. Make sure you choose wisely, you have a bad feeling somethign big is coming.', 'Gas Cloud', 'Skateboard', 'Fuji Apple', false];
 var finalBossEncounter = ['Welp, this is it. It doesnt look like your going to make it out of this one. That guy looks real big. You remind yourself that youve died once before, which calms you down momentarily, anticipating the impending ass kicking. Your move', 'This is the strength Option', 'this is the Intelligence Option', 'This is the agility option', true];
 encounterArray.push(dogEncounter, hipsterEncounter, firstDropEncounter, enviroOneEncounter, skeletonEncounter, secondDropEncounter, enviroTwoEncounter, ghostlyEncounter, thirdDropEncounter, finalBossEncounter);
-var Doggo = new Character('Doggo', 2, 2, 2, 20, true, 'assets/doggo.png');
-var hipsterGhost = new Character('Hipster Ghost', 3, 3, 3, 50, true, 'assets/hipster.png');
+var Doggo = new Character('Doggo', 2, 2, 2, 20, true, 'assets/doggo.png', 'music/battleOne.mp3');
+var hipsterGhost = new Character('Hipster Ghost', 3, 3, 3, 50, true, 'assets/hipster.png', 'music/battleOne.mp3');
 var firstItemDrop = new Character('Item Drop One', 506, 507, 508, 1, false);
 var enviroOne = new Character('Skeleton Trap', 500, 501, 502, 1, false);
-var Skeleton = new Character('Skeleton', 10, 7, 5, 20, true, 'assets/skullex.png');
+var Skeleton = new Character('Skeleton', 10, 7, 5, 20, true, 'assets/skullex.png', 'music/battleTwo.mp3');
 var secondItemDrop = new Character('Item Drop Two', 509, 5010, 5011, 1, false);
 var enviroTwo = new Character('Second Environment', 503, 504, 505, 1, false);
-var ghostly = new Character('Ghostly', 7, 14, 12, 22, true, 'assets/ghostly.png');
+var ghostly = new Character('Ghostly', 7, 14, 12, 22, true, 'assets/ghostly.png', 'music/battleTwo.mp3');
 var thirdItemDrop = new Character('Item Drop Three', 5012, 5013, 5014, 1, false);
-var finalBoss = new Character('Chaos Lord', 20, 13, 17, 30, true, 'assets/gho.png');
+var finalBoss = new Character('Chaos Lord', 20, 13, 17, 30, true, 'assets/gho.png', 'music/boss.wav');
 enemyCharacter.push(Doggo, hipsterGhost, firstItemDrop, enviroOne, Skeleton, secondItemDrop, enviroTwo, ghostly, thirdItemDrop, finalBoss);
 
 function appendUserGhost(){
